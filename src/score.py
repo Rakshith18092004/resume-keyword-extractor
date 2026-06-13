b4c929e56565e96d8bc88e995f887d955e9e25b0
Rakshith V <rakshith@v-align.in>
2026-06-13 20:08:47 +0530

    Return matched resume keywords

--- src/score.py
+++ src/score.py
@@ -14,3 +14,15 @@ def calculate_match(resume_keywords, jd_keywords):
     percentage = (len(matched) / len(jd_set)) * 100
 
     return round(percentage, 2)
+
+
+def get_matched_keywords(resume_keywords, jd_keywords):
+    """
+    Return the keywords that are present in both
+    the resume and the job description.
+    """
+
+    resume_set = set(resume_keywords)
+    jd_set = set(jd_keywords)
+
+    return list(resume_set.intersection(jd_set))
