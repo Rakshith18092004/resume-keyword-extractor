6e785ac06bd54d277b09c766a880803d3cc38883
Rakshith V <rakshith@v-align.in>
2026-06-13 20:05:01 +0530

    Implement keyword match scoring

---
+++ src/score.py
@@ -0,0 +1,16 @@
+def calculate_match(resume_keywords, jd_keywords):
+    """
+    Calculate the percentage of JD keywords found in the resume.
+    """
+
+    resume_set = set(resume_keywords)
+    jd_set = set(jd_keywords)
+
+    matched = resume_set.intersection(jd_set)
+
+    if len(jd_set) == 0:
+        return 0
+
+    percentage = (len(matched) / len(jd_set)) * 100
+
+    return round(percentage, 2)
